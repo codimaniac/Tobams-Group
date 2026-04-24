cff4bc7a9b68b0039c97a62f81148e04608a9e85
Anthony <150530087+codimaniac@users.noreply.github.com>
2026-04-24 22:26:10 +0100

    fix: missing nav bar on small screen

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -7,7 +7,7 @@ interface NavMenuProps {
 
 const NavMenu = ({className, isNavOpen}: NavMenuProps) => {
   return (
-    <div className={`flex xl:justify-center px-6 md:px-16 py-5 ${className} ${isNavOpen && !isNavOpen ? "hidden" : ""}`}>
+    <div className={`flex xl:justify-center px-6 md:px-16 py-5 ${className} ${!isNavOpen ? "hidden" : ""}`}>
         <div className="flex flex-col xl:flex-row gap-8">
             <NavMenuItem href="/">About</NavMenuItem>
             <NavMenuItem href="/what-we-do">What We Do</NavMenuItem>
